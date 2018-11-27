ea7f8112a8e6125b5638a4a1161a162ee62b59da
Jack <ajackofsometrades@gmail.com>
2018-11-26 17:46:34 -0800

    getcar and getcars have test coverage

--- src/data/state/reducers/__tests__/cars.test.js
+++ src/data/state/reducers/__tests__/cars.test.js
@@ -12,22 +12,28 @@ describe('reducer', () => {
     }
   };
 
-  it('returns an initialState', () => {
+  test('returns an initialState', () => {
     const newState = reducer(state, {});
     expect(newState).toEqual(state);
   });
 
-  it('handles a REGISTER_CAR and updates cars.list in state', () => {
+  test('handles a REGISTER_CAR and updates cars.list in state', () => {
     const newCar = fakeCarNew();
     const newState = reducer(state, {
       type: REGISTER_CAR,
       payload: newCar
     });
-    // expect(newState).toEqual({ ...state, cars: { ...state.cars, list: { ...state.cars.list, [expect.any(String)]: newCar } } });
     expect(Object.values(newState.cars.list)).toContainEqual(newCar);
+    expect(newState).toEqual({ ...state, cars: { ...state.cars, list: expect.any(Object) } });
+
+    // I know this is excessive, but I'm angry
+    const keys = Object.keys(newState.cars.list);
+    const newKey = keys.filter(key => newState.cars.list[key].plate == newCar.plate)[0];
+    expect(newState).toEqual({ ...state, cars: { ...state.cars, list: { ...state.cars.list, ...{ [newKey]: newCar } } } });
+
   });
 
-  it('handles a UPDATE_SEARCH and updates searchTerm in state', () => {
+  test('handles a UPDATE_SEARCH and updates searchTerm in state', () => {
     const searchTerm = 'asdf';
     const newState = reducer(state, {
       type: UPDATE_SEARCH,
--- src/data/state/selectors/__tests__/cars.test.js
+++ src/data/state/selectors/__tests__/cars.test.js
@@ -1,7 +1,36 @@
-import cars from '../cars.js';
+import {
+  getCar,
+  getCars,
+  searchCars
+} from '../cars';
 
-describe('cars selectors', () => {
-  test('null', () => {
-    expect(true).toBe(true);
+describe('cars selector', () => {
+  const state = {
+    cars: {
+      searchTerm: 'asdf',
+      list: {
+        abcd1234: { plate: 'abc123', make: 'Toyota', model: 'Tacoma', lotUses: 5, inLot: true },
+        decf6789: { plate: 'abc123', make: 'Toyota', model: 'Tacoma', lotUses: 3, inLot: false }
+      }
+    }
+  };
+
+  test('gets car from state', () => {
+    const car = getCar(state, 'abcd1234');
+    const carResult = { id: 'abcd1234', plate: 'abc123', make: 'Toyota', model: 'Tacoma', lotUses: 5, inLot: true };
+    expect(car).toEqual(carResult);
   });
+
+  test('gets all cars from state', () => {
+    const allCars = getCars(state);
+    const carResults = [
+      { id: 'abcd1234', plate: 'abc123', make: 'Toyota', model: 'Tacoma', lotUses: 5, inLot: true },
+      { id: 'decf6789', plate: 'abc123', make: 'Toyota', model: 'Tacoma', lotUses: 3, inLot: false }
+    ];
+    carResults.forEach(car => expect(allCars).toContainEqual(car));
+  });
+
+
+
+
 });
